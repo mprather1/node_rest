e5307b3d065c39a99fc3ddb7e459e6026c992b01
Michael Prather <mprather1@gmail.com>
2016-10-14 18:46:46 -0500

    deleteUser

--- server.js
+++ server.js
@@ -35,6 +35,17 @@ app.get("/users/:id", function(req, res) {
     });
 });
 
+var id = 1;
+
+app.get("/deleteUser", function(req, res) {
+    fs.readFile(__dirname + "/" + "users.json", "utf8", function(err, data) {
+        data = JSON.parse(data);
+        delete data["user" + id];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
+
 var server = app.listen(8000, function(){
   var host = server.address().address;
   var port = server.address().port;
