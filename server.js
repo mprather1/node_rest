2e9b0d16209bc40107384a96898ef64143f18086
Michael Prather <mprather1@gmail.com>
2016-10-14 11:13:23 -0500

    adduser

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 
+user = {
+  "user3" : {
+    "name" : "Adolf Hitler",
+    "password" : "HitlerDidNothingWrong",
+    "id" : 3
+  }
+};
+
 app.get("/listUsers", function(req, res){
   fs.readFile(__dirname + "/" + "users.json", "utf8", function(err, data){
     console.log(data);
     res.end(data);
+  });
+});
+
+app.get("/addUser", function(req, res){
+  fs.readFile(__dirname + "/" + "users.json", "utf8", function(err, data){
+    data = JSON.parse(data);
+    data["user3"] = user["user3"];
+    console.log(data);
+    res.end(JSON.stringify(data));
   });
 });
 
